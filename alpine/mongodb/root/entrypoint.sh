cf925373f7df6b242419ffae0be89c541185bc6e
Mikhail Bakulin <mikemadweb@gmail.com>
2018-01-18 17:39:49 +0300

    MongoDB image fixed

--- alpine/mongodb/root/entrypoint.sh
+++ alpine/mongodb/root/entrypoint.sh
@@ -9,7 +9,6 @@ MONGO_DATABASE=${MONGO_DATABASE:="db"}
 AUTH=${AUTH:="no"}
 
 set -m
-
 function initdb() {
     # Wait for MongoDB to boot
     RET=1
@@ -28,33 +27,31 @@ function initdb() {
     # If we've defined the MONGO_DATABASE environment variable and it's a different database
     # than admin, then create the user for that database.
     if [ "$MONGO_DATABASE" != "admin" ]; then
-        echo "=> Creating '${MONGO_DATABASE}' database in MongoDB"
         echo "=> Creating '${MONGO_USER}' user with '${MONGO_PASS}' password in MongoDB"
+        echo "=> Using ${MONGO_DATABASE} database"
         mongo admin -u ${MONGO_ROOT_USER} -p ${MONGO_ROOT_PASS} << EOF
-    echo "Using ${MONGO_DATABASE} database"
-    use ${MONGO_DATABASE}
-    db.createUser({user: '${MONGO_USER}', pwd: '${MONGO_PASS}', roles:[{role:'dbOwner', db:'${MONGO_DATABASE}'}]})
+    use ${MONGO_DATABASE};
+    db.createUser({user: '${MONGO_USER}', pwd: '${MONGO_PASS}', roles:[{role:'dbOwner', db:'${MONGO_DATABASE}'}]});
     use admin;
     db.shutdownServer()
 EOF
     fi
-    sleep 1
 
     # If everything went well, add a file as a flag so we know in the future to not re-create the
     # users if we're recreating the container (provided we're using some persistent storage)
     touch /data/.mongodb_password_set
     echo "MongoDB configured successfully. Database will restart."
-    exit 1
+    sleep 10
 }
 
 cmd="$@"
 if [ ! -f /data/.mongodb_password_set ]; then
     $cmd &
     initdb
-else
-    if [ "$AUTH" == "yes" ]; then
-        cmd="$cmd --auth"
-    fi
-
-    exec $cmd;
 fi
+
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
+
+exec $cmd;
